e7fa38840b010bdae4b72081e3b7f30e3444a834
s20164046 <s20164046@HCS60105.stdlan.hcs.local>
2019-11-01 11:03:38 +0900

    index.html初期遷移

--- helloworld_project/src/main/java/info/helloworld/sbmt/web/MainController.java
+++ helloworld_project/src/main/java/info/helloworld/sbmt/web/MainController.java
@@ -1,5 +1,15 @@
 package info.helloworld.sbmt.web;
 
-public class MainController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+@Controller
+public class MainController {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String index(Model model) {
+		model.addAttribute("message", "Hello Springboot");
+		return "index";
+	}
 }
