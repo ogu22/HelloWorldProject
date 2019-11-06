d270bfc03b8711639202f797b30718024d414885
riku <rrr.naganawa@gmail.com>
2019-11-06 10:29:39 +0900

    もうだめだ

--- helloworld_project/src/main/java/info/helloworld/sbmt/config/SecurityConfig.java
+++ helloworld_project/src/main/java/info/helloworld/sbmt/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package info.helloworld.sbmt.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +12,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+import dto.UserDto;
 
 @Configuration
 @EnableWebSecurity
@@ -40,11 +44,35 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 				.csrf();
 	}
 
+	 @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+	        UserDto support = supportUser();
+	        UserDto admin = adminUser();
+
+	        auth.inMemoryAuthentication()
+	                .withUser(support.getUsername()).password(support.getPassword()).roles(ROLE_USER)
+	                .and()
+	                .withUser(admin.getUsername()).password(admin.getPassword()).roles(ROLE_ADMIN);
+	    }
+	
 	@Bean
 	public SpringTemplateEngine templateEngine() {
 	    SpringTemplateEngine engine = new SpringTemplateEngine();
 	    engine.addDialect(new SpringSecurityDialect());
 	    engine.setTemplateResolver(templateResolver);
 	    return engine;
-	}
+	} 
+	@Bean
+    @ConfigurationProperties("inmotion.admin")
+    public UserDto adminUser() {
+        return new UserDto();
+    }
+
+    @Bean
+    @ConfigurationProperties("inmotion.user")
+    public UserDto supportUser() {
+        return new UserDto();
+    }
+    
+    
 }
