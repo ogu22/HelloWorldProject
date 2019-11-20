ee52df0bc139b3496334c8b5cfa2b535f4845b86
ogu22 <s.oguma22@gmail.com>
2019-11-20 13:50:03 +0900

    一時コミット

--- helloworld_project/src/main/java/info/helloworld/sbmt/domain/User.java
+++ helloworld_project/src/main/java/info/helloworld/sbmt/domain/User.java
@@ -1,5 +1,46 @@
 package info.helloworld.sbmt.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+	@Id
+	private Long userId;
+	private String userName;
+	private String password;
+	private String classId;
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getClassId() {
+		return classId;
+	}
+
+	public void setClassId(String classId) {
+		this.classId = classId;
+	}
 
 }
--- helloworld_project/src/main/java/info/helloworld/sbmt/domain/UserRepository.java
+++ helloworld_project/src/main/java/info/helloworld/sbmt/domain/UserRepository.java
@@ -1,5 +1,9 @@
 package info.helloworld.sbmt.domain;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
